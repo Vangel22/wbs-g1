479399f6b0f89e46ffd496c287aecae3dd4983a5
Vangel22 <h.vangel22@gmail.com>
2024-08-23 20:41:17 +0200

    save before break

--- c02/v1/handlers/cars.js
+++ c02/v1/handlers/cars.js
@@ -1,5 +1,66 @@
-const getCars = async (req, res) => {};
+const { readFile, writeFile } = require("../read-write");
+
+const getCars = async (req, res) => {
+  try {
+    const cars = await readFile("data.json");
+    return res.status(200).send(cars);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const addCar = async (req, res) => {
+  try {
+    const cars = await readFile("data.json");
+    const newCar = req.body;
+    cars.push(newCar);
+    await writeFile("data.json", cars);
+    return res.status(200).send("New car added!");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const updateCar = async (req, res) => {
+  try {
+    // vo req.params go imame identifikatorot za odreden avtomobil
+    let cars = await readFile("data.json");
+    const carId = Number(req.params.id); // 0
+    const newData = req.body; // year 2019
+
+    cars = cars.map((car, index) => {
+      // 0 -> Mercedes
+      if (index === carId) {
+        return {
+          ...car, // brand: Mercedes, model: CLS, year: 2020
+          ...newData, // year: 2019
+        };
+      }
+      return car;
+    });
+    await writeFile("data.json", cars);
+    return res.status(200).send("Car updated successfully!");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+const removeCar = async (req, res) => {};
 
 module.exports = {
   getCars,
+  addCar,
+  updateCar,
+  removeCar,
 };
+
+// const objReq = {
+//     params: {
+//         id: 0
+//     }
+// }
+
+// const { id } = objReq.params;
--- c02/v1/index.js
+++ c02/v1/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 
+const { getCars, addCar, updateCar } = require("./handlers/cars");
+
 const app = express();
+app.use(express.json());
+// ova ni treba za da mozeme da ispratime req.body vo json format
 
 app.get("/cars", getCars);
+// app.get("/cars", () => { console.log("Nema da ja fati ovaa funkcija") });
+app.post("/cars", addCar);
+app.put("/cars/:id", updateCar);
 
 app.listen(3000, () => console.log("Server started at port 3000!"));
