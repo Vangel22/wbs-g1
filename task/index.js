42eec46eda4d8fbca520d886288698eabcdfcd6b
Vangel22 <h.vangel22@gmail.com>
2024-09-04 20:31:58 +0200

    Save task for class 06

---
+++ task/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { expressjwt: jwt } = require("express-jwt");
+
+// const testObj = {
+//   name: "Test"
+// }
+
+// const { name: ime } = testObj;
+// console.log('name', name);
+
+const connectDB = require("./models/db/config");
+connectDB();
+
+const { getSection } = require("./models/config");
+const {
+  login,
+  register,
+  refreshToken,
+  resetPassword,
+} = require("./controllers/auth");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  jwt({
+    secret: getSection("development").jwt_secret,
+    algorithms: ["HS256"],
+  }).unless({
+    path: ["/auth/login", "/auth/register", "/auth/reset"],
+  })
+);
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.post("/auth/login", login);
+app.post("/auth/register", register);
+app.post("/auth/refresh", refreshToken);
+app.post("/auth/reset", resetPassword);
+
+app.listen(getSection("development").port, () =>
+  console.log(`Server started at port ${getSection("development").port}!`)
+);
